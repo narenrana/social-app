31d4c44f24f303104f35c89e9f846c2f2f6afd16
Narender <narenrana02@gmail.com>
2019-12-17 22:20:15 +0800

    skip test

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,10 @@
 #Go to social app project and run this
 
-mvn clean install
+mvn clean install -Dmaven.test.skip=true
 
 docker build --build-arg  JAR_FILE=target/*.jar -t spgroup-sample .
 
-docker run -p 8080:8080 spgroup-sample
+docker run -p 80:8080 spgroup-sample
 
 
 
